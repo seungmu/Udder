7e511247770436992fc4ede102815b099fce116e
seungmu <jerome_zheng@outlook.com>
2020-07-09 08:11:58 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Seungmu Jeng on 2020/7/9.
+//  Copyright © 2020 Seungmu Jeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
